42d516ea547ca81423c89f9a0837fc3a871c574d
Víctor <129689618+vmspinelli@users.noreply.github.com>
2023-05-12 00:48:31 -0300

    Varios cambios, además del form de contacto, vean la descripción
    FORMULARIO DE CONTACTO (tiene otra fuente, véanlo y me dicen qué les parece). hice un nuevo css de estilos del form porque lo trabajé aparte del que ya había para no hacer lío, dsp lo reemplacé Aun tengo que agarrar el de reserva, si les gusta el de contacto intentaré estilizarlo igual. h
    
    LIMPIÉ UN POCO LOS HTML: pasé footer y nav a un app.js y lo llamé con un <script src="#">, para no tener que repetir el código en cada página
    
    Creo que nada más. Igual tengo aun que animar el botón de ENVIAR.

---
+++ app.js
@@ -0,0 +1,23 @@
+let header = `
+<header>
+ <nav>
+  <ul>
+    <li><a href="MainPage.html">Renta de Autos</a></li>
+    <li><a href="Disponibles.html">Autos disponibles</a></li>
+    <li><a href="Reserva.html">Reservar</a></li>
+    <li><a href="contacto.html">Contacto</a></li>
+  </ul>
+</nav>
+</header>`
+
+let footer = `<footer>
+<div>
+    <p>Contacto: rentadeautos@example.com</p>
+</div>
+<div>
+    <p class="p-block">© 2023 RentCarLux</p>
+</div>
+</footer>`
+
+document.write(header)
+document.write(footer)
