34552decccb6fab0d1f67f7238a67886641dadd7
Víctor <129689618+vmspinelli@users.noreply.github.com>
2023-05-21 23:56:08 -0300

    Cambio en sintaxis de "alerts" del formulario

--- js/form.js
+++ js/form.js
@@ -14,14 +14,14 @@ function checkInputs() {
     var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
 
     if (nombre === '' || apellido === '' || email === '' || mensaje === '') {
-        alert('Por favor, complete todos los campos obligatorios marcados con un asterisco.');
+        alert(`Por favor, complete todos los campos obligatorios marcados con un asterisco.`);
         return false;
     }
 
     for (var i = 0; i < nombre.length; i++) {
         var charCode = nombre.charCodeAt(i);
         if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32 || charCode === 225 || charCode === 233 || charCode === 237 || charCode === 243 || charCode === 250)) {
-            alert('El campo "Nombre" sólo puede contener letras y espacios.');
+            alert(`El campo "Nombre" sólo puede contener letras y espacios.`);
             return false;
         }
     }
@@ -29,15 +29,15 @@ function checkInputs() {
     for (var i = 0; i < apellido.length; i++) {
         var charCode = apellido.charCodeAt(i);
         if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32 || charCode === 225 || charCode === 233 || charCode === 237 || charCode === 243 || charCode === 250)) {
-            alert('El campo "Apellido" sólo puede contener letras y espacios');
+            alert(`El campo "Apellido" sólo puede contener letras y espacios`);
             return false;
         }
 
     }
     if (!email.match(validEmail)) {
-        alert('E-Mail incorrecto\nEl formato debe ser: usuario@dominio.xxx')
+        alert(`E-Mail incorrecto\nEl formato debe ser: usuario@dominio.xxx`)
         return false
     }
-    alert("Mensaje enviado!\n\nGracias " + nombre + " " + apellido + ", le responderemos cuanto antes a su email: " + email);
+    alert(`Mensaje enviado!\n\nGracias ${nombre} ${apellido}, le responderemos cuanto antes a su email: ${email}`);
     formulario.reset();
 }
