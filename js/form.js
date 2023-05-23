1b7982a9124e2221ce81b83d4703c4c102969729
Víctor <129689618+vmspinelli@users.noreply.github.com>
2023-05-23 01:46:20 -0300

    Form RESPONSIVE

--- js/form.js
+++ js/form.js
@@ -4,6 +4,7 @@ form.addEventListener('submit', e => {
     checkInputs();
 })
 // FUNCIÓN CON LAS VALIDACIONES
+
 function checkInputs() {
     // CAPTURAMOS EN VARIABLES LOS VALUES QUE INGRESA EL USUARIO EN CADA CAMPO
     var formulario = document.getElementById("form");
@@ -14,14 +15,17 @@ function checkInputs() {
     var mensaje = document.getElementById("msg").value.trim();
 
     // CAPTURAMOS EN UNA VARIABLE LA EXPRESIÓN REGULAR QUE REPRESENTA EL FORMATO CORRECTO DEL EMAIL
-    var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+
+    var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
     // CHEQUEAMOS QUE NO HAYA CAMPOS VACÍOS
+
     if (nombre === '' || apellido === '' || email === '' || mensaje === '') {
         alert(`Por favor, complete todos los campos marcados con un asterisco.`);
         return false;
     }
     // VALIDAMOS NOMBRE
+
     for (var i = 0; i < nombre.length; i++) {
         var charCode = nombre.charCodeAt(i);
         if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32 || charCode === 225 || charCode === 233 || charCode === 237 || charCode === 243 || charCode === 250)) {
@@ -30,22 +34,24 @@ function checkInputs() {
         }
     }
     // VALIDAMOS APELLIDO
+
     for (var i = 0; i < apellido.length; i++) {
         var charCode = apellido.charCodeAt(i);
         if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32 || charCode === 225 || charCode === 233 || charCode === 237 || charCode === 243 || charCode === 250)) {
             alert(`El campo "Apellido" sólo puede contener letras y espacios`);
             return false;
         }
-
     }
     // VALIDAMOS EMAIL
-    if (!email.match(validEmail)) {
-        alert(`E-Mail incorrecto\nEl formato debe ser: usuario@dominio.xxx`)
-        return false
-    }
-    // SI TODO ESTÁ BIEN, SALTA ALERT AVISANDO QUE EL MENSAJE SE HA ENVIADO Y SE RESETEAN TODOS LOS CAMPOS 
-    alert(`Mensaje enviado!\n\nGracias ${nombre} ${apellido}, le responderemos cuanto antes a su email ${email}`);
-    formulario.reset();
-}
 
-// FALTA HACERLO FUNCIONAL, QUE SE ENVÍE EFECTIVAMENTE A UN MAIL. ENCONTRÉ UNA API PARA HACERLO, PERO POR ALGUNA RAZÓN, SI ESTÁ ACTIVAD ESTA VALIDACIÓN NO FUNCIONA. PROBÉ UBICAR EL LLEMADO <SCRIPT> EN VARIOS LUGARES, PERO AL APRECER ALGO BLOQUEA LA EJECUCIÓN CORRECTA DE LA API
+    if (!email.match(validEmail)) {
+        alert(`E-Mail incorrecto\nEl formato debe ser: usuario@dominio.xxx`);
+        return false;
+    }
+    // SI TODO ESTÁ BIEN, DEBERÍA ENVIARSE. PERO NOOOO ANDAAAA!
+
+    else {
+        alert(`Muchas gracias por contactactarnos ${nombre}, nos comunicaremos con Ud. cuanto antes`);
+        formulario.submit();
+    }
+}
