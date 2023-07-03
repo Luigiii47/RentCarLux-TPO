62dd9864a1462e93c6f66e389885745009122000
Víctor <129689618+vmspinelli@users.noreply.github.com>
2023-07-03 02:14:55 -0300

    Creacion de DB y clase AUTO

---
+++ CRUD/app.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+# Configurar la conexión a la base de datos SQLite
+DATABASE = 'vehiculos.db'
+
+# Obtener la conexión a la base de datos
+
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+# Crear la tabla 'productos' si no existe
+conn = get_db_connection()
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS autos (
+        codigo INTEGER PRIMARY KEY AUTOINCREMENT,
+        cantidad INTEGER NOT NULL,
+        marca VARCHAR(50),
+        modelo VARCHAR(50),
+        motor VARCHAR(50),
+        potencia INTEGER,
+        velocidad INTEGER,
+        peso FLOAT,
+        color VARCHAR(50)
+    )
+''')
+conn.commit()
+cursor.close()
+conn.close()
+
+# -------------------------------------------------------------------
+# Definimos la clase "Auto"
+# -------------------------------------------------------------------
+
+class Auto:
+    def __init__(self, codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color):
+        self.codigo = codigo
+        self.cantidad = cantidad
+        self.marca = marca
+        self.modelo = modelo
+        self.motor = motor
+        self.potencia = potencia
+        self.velocidad = velocidad
+        self.peso = peso
+        self.color = color
+
+    def modificar(self, codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color):
+        self.codigo = nuevo_codigo
+        self.cantidad = nuevo_cantidad
+        self.marca = nuevo_marca
+        self.modelo = nuevo_modelo
+        self.motor = nuevo_motor
+        self.potencia = nuevo_potencia
+        self.velocidad = nuevo_velocidad
+        self.peso = nuevo_peso
+        self.color = nuevo_color
