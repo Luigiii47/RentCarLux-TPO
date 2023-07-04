45bf2156b1ae1c36b0239bc8b08a3176ba66db96
ramiro13 <105287515+ramiro13@users.noreply.github.com>
2023-07-03 21:45:08 -0300

    cositas nuevas

--- CRUD/app.py
+++ CRUD/app.py
@@ -60,3 +60,100 @@ class Auto:
         self.color = nuevo_color
 
 
+class Stock:
+    def __init__(self):
+        self.conexion = get_db_connection()
+        self.cursor = self.conexion.cursor()
+
+    def agregar_auto(self, codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color):
+        producto_existente = self.consultar_producto(codigo)
+        if producto_existente:
+            print("Producto existente")
+            return False
+        
+        self.cursor.execute("INSERT INTO productos VALUES (?, ?, ?, ?)", ( codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color))
+        self.conexion.commit()
+        return True
+    
+
+    # Busqueda de producto por motor
+
+    def consultar_producto_codigo(self, motor):
+        self.cursor.execute("SELECT * FROM productos WHERE codigo = ?", (motor,))
+        row = self.cursor.fetchone()
+        if row:
+            codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color= row
+            return Auto(codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color)
+        return False
+    
+    # Busqueda de producto por marca
+
+    def consultar_producto_codigo(self, marca):
+        self.cursor.execute("SELECT * FROM productos WHERE codigo = ?", (marca,))
+        row = self.cursor.fetchone()
+        if row:
+            codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color= row
+            return Auto(codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color)
+        return False
+    
+
+    # Busqueda de producto por modelo
+
+    def consultar_producto_codigo(self, modelo):
+        self.cursor.execute("SELECT * FROM productos WHERE codigo = ?", (modelo,))
+        row = self.cursor.fetchone()
+        if row:
+            codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color= row
+            return Auto(codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color)
+        return False
+    
+    # Busqueda de producto por color
+
+    def consultar_producto_codigo(self, color):
+        self.cursor.execute("SELECT * FROM productos WHERE codigo = ?", (color,))
+        row = self.cursor.fetchone()
+        if row:
+            codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color= row
+            return Auto(codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color)
+        return False
+
+    
+
+    def modificar_producto(self,codigo, nuevo_cantidad, nuevo_marca, nuevo_modelo, nuevo_motor, nuevo_potencia, nuevo_velocidad, nuevo_peso, nuevo_color):
+        producto = self.consultar_producto(codigo)
+        if producto:
+            producto.modificar(nuevo_cantidad, nuevo_marca, nuevo_modelo, nuevo_motor, nuevo_potencia, nuevo_velocidad, nuevo_peso, nuevo_color)
+            self.cursor.execute("UPDATE productos SET descripcion = ?, cantidad = ?, precio = ? WHERE codigo = ?",
+                                (nuevo_cantidad, nuevo_marca, nuevo_modelo, nuevo_motor, nuevo_potencia, nuevo_velocidad, nuevo_peso, nuevo_color, codigo))
+            self.conexion.commit()
+
+
+    def listar_productos(self):
+        print("-" * 30)
+        self.cursor.execute("SELECT * FROM productos")
+        rows = self.cursor.fetchall()
+        for row in rows:
+            codigo, cantidad, marca, modelo, motor, potencia, velocidad, peso, color = row
+            print(f"Código: {codigo}")
+            print(f"Marca: {marca}")
+            print(f"Modelo: {modelo}")
+            print(f"Cantidad: {cantidad}")
+            print(f"Motor: {motor}")
+            print(f"Potencia: {potencia}")
+            print(f"Velocidad: {velocidad}")
+            print(f"Peso : {peso}")
+            print(f"Color: {color}")
+            print("-" * 30)
+
+    def eliminar_producto(self, codigo):
+        self.cursor.execute("DELETE FROM productos WHERE codigo = ?", (codigo,))
+        if self.cursor.rowcount > 0:
+            print("Producto eliminado.")
+            self.conexion.commit()
+        else:
+            print("Producto no encontrado.")
+    
+
+
+
+    
